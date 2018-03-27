dcc4ed7e3d9af4754e3160d3ebc561b1ee1875c4
AndrewBiscuit <andrewalford1@outlook.com>
2018-03-27 20:50:50 +0100

    Attempted to do work, keyboard wont work properly

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "mbed.h"
 #include "components.h"
+#include <cmath>
 
 AssignmentBoard board;
 
@@ -8,26 +9,63 @@ LED green(board.SHIELD_GREEN_LED);
 
 Speaker speaker(board.SHIELD_SPEAKER);
 
+float getFrequency(float key)
+{
+  float frequency = pow(2.0, ((key - 49)/12)) * 440;
+
+  printf("Key %f = frequecncy: %f \n",  key, frequency );
+
+  return frequency;
+}
+
+void playNote(float key, float length)
+{
+
+
+  speaker.pwmOut.period(1/getFrequency(key));
+  speaker.play();
+
+  wait(length);
+}
+
 int main()
 {
     while (1)
     {
-        float f;
 
         green.on();
 
-        for (f=20.0; f<20.0e3; f+=100)
-        {
-            speaker.pwmOut.period(1.0/f);
+        // for (f=20.0; f<20.0e3; f+=100)
+        // {
+        //     speaker.pwmOut.period(1.0/f);
+        //
+        //     speaker.play();
+        //
+        //     wait(0.1);
+        // }
 
-            speaker.play();
 
-            wait(0.1);
-        }
+        playNote(66, 2); //D
+        playNote(66, 2); //A
+        playNote(71, 0.5) //G
+        playNote(69, )
+
+
+        // playNote(64); //C
+        // playNote(63); //B
+        // playNote(61); //A
+        // playNote(66); //D
+        // playNote(64); //C
+        // playNote(63); //B
+        // playNote(61); //A
+        // playNote(66); //D
+
+
+        green.off();
 
         speaker.stop();
 
-        green.off();
+
 
         while(!(fire.digitalIn));
     }
